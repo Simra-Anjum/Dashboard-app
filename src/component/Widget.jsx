46f504fb8f66cd97efd2ccd107e7bb7d027933d8
Simra-Anjum <simraanjum68@gmail.com>
2025-09-27 11:29:26 +0530

    Update Widget.jsx

--- src/component/Widget.jsx
+++ src/component/Widget.jsx
@@ -5,8 +5,9 @@ const Widget = ({ widget, category }) => {
   const { removeWidget } = useContext(DashboardContext);
 
   return (
-    <div className="bg-white rounded-lg w-[32%] min-h-[220px] px-3 py-4 shadow-lg flex flex-col  transform transition duration-300 hover:scale-105">
-    
+    <div className="bg-white rounded-lg w-full sm:w-[48%] lg:w-[32%] min-h-[220px] px-3 py-4 shadow-lg flex flex-col transform transition duration-300 hover:scale-105">
+      
+  
       <div className="flex relative rounded-lg bg-gray-100 p-2">
         <h3 className="font-semibold text-sm">{widget.title}</h3>
         <button
@@ -17,7 +18,7 @@ const Widget = ({ widget, category }) => {
         </button>
       </div>
 
-     
+
       <div className="flex items-center justify-center flex-grow p-2">
         <p className="text-gray-700 text-sm text-center">{widget.content}</p>
       </div>
